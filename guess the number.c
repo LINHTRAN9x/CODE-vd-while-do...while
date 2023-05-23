c5c451c407ab1ce2fb85f31077bb15ac9fd5be6b
Trần Văn Linh <133183567+LINHTRAN9x@users.noreply.github.com>
2023-05-23 10:30:33 +0700

    guess the number.c

--- guess the number.c
+++ guess the number.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 int main() {
-    srand(time(NULL));
+    srand(time(NULL));//khơi tạo seed cho hàm rand() bằng thời gian hiện tại,se ramdom mỗi lần chạy program.
     int guess;
     int random;
-     random= rand ()%100+1 ;
+     random= rand ()%100+1 ;//hàm rand() sinh ra số ngẫu nhiên.
     printf("Guess the number (1-100): ");
-    scanf("%d",&guess);
+    scanf("%d",&guess);//nhập số guess
     do {
-
         if (guess >= random)
             printf("To high\n");
-
         else
                 printf("To low\n");
-    printf("Guess the number again(1-100): ");
+        
+    printf("Guess the number again(1-100): ");//vòng lặp lại yêu cầu nhập lại guess number
     scanf("%d",&guess);
 
-}while (guess != random);
+}while (guess != random);//Nếu guess không bằng số ramdom thì sẽ lặp lại.nếu bằng random sẽ kết thúc chương trình.
     printf("congratulation!!!! ");
 
     return 0;
