31b8aef6b2bd9c120e73ec3025cd44bf277dd1b7
Trần Văn Linh <133183567+LINHTRAN9x@users.noreply.github.com>
2023-05-23 09:14:08 +0700

    Add files via upload

---
+++ code vd while.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int num; //mặc định num = 0
+
+    while(num <= 5){      // Nếu num nhỏ hơn or bằng 5,thì in ra màn hình theo thứ tự tăng dần.
+        printf("%d\n",num);
+        num++; // num +1
+    }
+return 0;
+}
